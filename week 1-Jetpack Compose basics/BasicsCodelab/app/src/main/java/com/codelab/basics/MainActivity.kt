6c24fa2da3219fc7fdae515b8b526dfbffd50a1b
cpstudy <guide94@naver.com>
2021-11-07 16:02:21 +0900

    모든 경고를 에러로 띄우는 옵션 제거

--- week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -50,9 +50,7 @@ private fun MyApp() {
             shouldShowOnBoarding = false
         }
     } else {
-        Surface {
-            Text("apple")
-        }
+        LayoutCodelab()
     }
 }
 
