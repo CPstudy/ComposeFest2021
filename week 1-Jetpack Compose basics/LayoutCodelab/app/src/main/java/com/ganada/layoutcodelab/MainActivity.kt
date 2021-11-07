cece73bc502a0807760877ce0d13fba715d9723b
cpstudy <guide94@naver.com>
2021-11-07 20:31:56 +0900

    LayoutCodlab

---
+++ week 1-Jetpack Compose basics/LayoutCodelab/app/src/main/java/com/ganada/layoutcodelab/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.ganada.layoutcodelab
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ganada.layoutcodelab.ui.theme.LayoutCodelabTheme
+import com.ganada.layoutcodelab.ui.theme.Purple200
+import com.ganada.layoutcodelab.ui.theme.Purple700
+import com.google.android.material.color.MaterialColors
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LayoutCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    PhotographerCard(
+                        modifier = Modifier
+                            .padding(start = 24.dp)
+                            .background(
+                                color = Purple700
+                            )
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LayoutCodelabTheme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(modifier
+        .padding(8.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .background(MaterialTheme.colors.surface)
+        .clickable(onClick = { /* Ignoring onClick */ })
+        .padding(16.dp)
+    ) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+
+        }
+        Column(
+            modifier = modifier
+                .align(Alignment.CenterVertically)
+                .background(
+                    color = Purple200
+                )
+        ) {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            // LocalContentAlpha is defining opacity level of its children
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
+    LayoutCodelabTheme {
+        PhotographerCard()
+    }
+}
